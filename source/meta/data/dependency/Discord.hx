e024719b40abbd47365555b0cdd3d5bfedc0b42d
DANIZIN <98532797+DANIZIN23@users.noreply.github.com>
2023-07-17 23:38:24 -0300

    Update Discord.hx

--- source/meta/data/dependency/Discord.hx
+++ source/meta/data/dependency/Discord.hx
@@ -1,11 +1,10 @@
 
 #if desktop
 package meta.data.dependency;
-#end
 import lime.app.Application;
 import meta.state.PlayState;
 import discord_rpc.DiscordRpc;
-
+#end
 /**
 	Discord Rich Presence, both heavily based on Izzy Engine and the base game's, as well as with a lot of help 
 	from the creator of izzy engine because I'm dummy and dont know how to program discord
